1137feab4a7deb730dd3dbea27c87fd2222daac3
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-07-11 13:09:06 -0700

    [Feature] Adding a BaseModel class

---
+++ models/base_model.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+"""
+Module that defines parent class for other classes
+"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel():
+    """Class that defines all common attributes/methods
+    for others classes
+    """
+
+    def __init__(self):
+        """Initialize attributes"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """Returns a string for printing a class instance
+        """
+        cls_name = "[" + self.__class__.__name__ + "]"
+        dic = {k: v for (k, v) in self.__dict__.items() if (not v) is False}
+        return (cls_name + " (" + self.id + ") " + str(dic))
+
+    def save(self):
+        """Updates the public instance attribute updated_at
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionnary containing all keys/values of
+        __dict__ of the instance"""
+
+        new_dict = {}
+        fmat = "%Y-%m-%dT%H:%M:%S.%f"
+        for (k, v) in self.__dict__.items():
+            if k == "created_at" or k == "updated_at":
+                new_dict[k] = v.strftime(fmat)
+            else:
+                if not v:
+                    pass
+                else:
+                    new_dict[k] = v
+
+        new_dict['__class__'] = self.__class__.__name__
+
+        return new_dict
