f7fe750a3126e4898c7368cbfefba308776ff94c
Ige Victor <victorkoladeige@gmail.com>
2023-07-15 08:27:50 -0700

    class city documentation updated

--- models/city.py
+++ models/city.py
@@ -4,6 +4,10 @@ from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """ Define the City class attribute"""
+    """ Define the City class attribute
+    Public class attributes:
+    state_id: string - empty string
+    name: string - empty string
+    """
     state_id = ""
     name = ""
